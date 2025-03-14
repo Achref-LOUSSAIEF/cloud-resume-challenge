528a8ae711c7b728c1bd61091e1c9f6a3a6d3b05
Achref LOUSSAIEF <achref.loussaief@telecom-sudparis.eu>
2025-03-14 12:18:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+$(document).ready(function (e) {
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    // resize event
+    $win.resize(function () {
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function (e) {
+        $navbar.toggleClass("toggle-left");
+    });
+});
+
+function toggle_onclick($win, $navbar, width) {
+    if ($win.width() <= 768) {
+        $navbar.css({ left: `-${width}px` });
+    } else {
+        $navbar.css({ left: '0px' });
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// This function fetches the visitor count and updates the HTML content
+function updateCounter() {
+    fetch('https://odoetmt010.execute-api.us-east-1.amazonaws.com/')
+      .then(res => res.json())
+      .then(res => {
+        console.log("API Response:", res); // Debugging
+        document.getElementById("counter").innerHTML = `👀 Views: ${res.visitor_count}`;
+      })
+      .catch(error => console.error("Fetch Error:", error));
+  }
+  
+  // Wait for the DOM to load before calling the function
+  document.addEventListener('DOMContentLoaded', function() {
+    updateCounter();
+  });
+  
